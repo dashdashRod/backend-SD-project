dbfea26dc0f18e5f730928103a92ba3cdf35b150
Leoni Kurt <leonikurt@gmail.com>
2020-04-13 23:54:41 -0300

    initializing project

---
+++ src/main/java/com/backend/sd/sdbackend/SdbackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sd.sdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/sd/sdbackend/SdbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sd.sdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
